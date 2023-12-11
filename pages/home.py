5e7f8c77b37a716c55cefdf95aed52f3a735066d
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-12-12 00:48:35 +1100

    completed home view

--- pages/home.py
+++ pages/home.py
@@ -1,4 +1,3 @@
-from tkinter import filedialog
 from tkinter import *
 
 import router
@@ -9,25 +8,24 @@ class Home(page_class.Page):
     def __init__(self, state):
         page_class.Page.__init__(self, state, 200, 200)
         self.invoice_info()
-        self.save_dir()
 
     def invoice_info(self):
         frame = Frame(self)
-        frame.grid(row=0, column=0, sticky="w")
+        frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
         date_lbl = Label(master=frame, text="Invoice Date")
         num_lbl = Label(master=frame, text="Invoice Number")
-        date_lbl.grid(row=0, column=0)
-        num_lbl.grid(row=0, column=1)
+        date_lbl.grid(row=0, column=0, sticky="w")
+        num_lbl.grid(row=1, column=0, sticky="w")
 
         date_ent = Entry(master=frame)
         num_ent = Entry(master=frame)
-        date_ent.grid(row=1, column=0)
+        date_ent.grid(row=0, column=1)
         num_ent.grid(row=1, column=1)
-    
-    def save_dir(self):
-        frame = Frame(self)
-        frame.grid(row=1, column=0)
 
-        get_dir = Button(master=frame, text="Choose save location", command=lambda : router.change_view(self, add_manifest.AddManifest, self.state))
-        get_dir.grid()
+        man_page_btn = Button(master=frame, text='Start Adding Manifests', command=lambda : self.nav_add_manifests(date_ent.get(), num_ent.get()))
+        man_page_btn.grid(row=3, column=0)
+    
+    def nav_add_manifests(self, date, num):
+        self.model.set_inv_info(date, num)
+        router.change_view(self, add_manifest.AddManifest, self.state)
