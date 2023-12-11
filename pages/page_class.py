35e36117d79c68e64730c8bbc10a78aaef2eed05
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-12-12 00:42:15 +1100

    page class now sets model field

--- pages/page_class.py
+++ pages/page_class.py
@@ -4,6 +4,7 @@ class Page(Frame):
     def __init__(self, state, width, height):
         Frame.__init__(self)
         self.state = state
+        self.model = state.get_model()
         self.root = state.get_root()
         self.width = width
         self.height = height
