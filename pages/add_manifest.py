468b4c9fadf4d5da9241395f894ce30ed9b5cbc3
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-12-12 00:47:40 +1100

    added buttons to save csv and add manifest info

--- pages/add_manifest.py
+++ pages/add_manifest.py
@@ -6,7 +6,6 @@ class AddManifest(page_class.Page):
     def __init__(self, state):
         page_class.Page.__init__(self, state, 300, 700)
         self.manifest_info()
-        self.load_input()
 
     def manifest_info(self):
         frame = Frame(self.root)
@@ -22,19 +21,18 @@ class AddManifest(page_class.Page):
         man_no_ent = Entry(master=frame)
         man_date_ent.grid(row=1, column=0)
         man_no_ent.grid(row=1, column=1)
-    
-    def load_input(self):
-        frame = Frame(self.root)
-        frame.grid(row=1, column=0, sticky="w")
 
         str_no_lbl = Label(master=frame, text="Store Number")
-        str_no_lbl.grid(row=0, column=0)
+        str_no_lbl.grid(row=2, column=0)
 
         load_info = []
-        for i in range(1, 4):
+        for i in range(4, 7):
             str_no_ent = Entry(master=frame)
             str_no_ent.grid(row=i, column=0)
             load_info.append(str_no_ent)
 
-        add_ent = Button(master=frame, text="Add Manifest", command=lambda : print("hey"))#main.add_manifest(load_info, num_ent.get(), mandate_ent.get(), date_ent.get(), manno_ent.get()))
-        add_ent.grid(row=4, column=0, sticky="w")
+        add_ent_btn = Button(master=frame, text="Add Manifest", command=lambda : self.model.add_manifest(load_info, man_date_ent.get(), man_no_ent.get()))
+        add_ent_btn.grid(row=7, column=0, sticky="w")
+
+        save_csv_btn = Button(master=frame, text='Save CSV', command=self.model.save_csv)
+        save_csv_btn.grid(row=7, column=1)
