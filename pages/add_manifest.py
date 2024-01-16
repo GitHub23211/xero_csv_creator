4685000ffcc98672386f5bc818e26ff37a633e68
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-01-16 16:02:16 +1100

    redid whole page

--- pages/add_manifest.py
+++ pages/add_manifest.py
@@ -4,71 +4,79 @@ from pages import page_class
 
 class AddManifest(page_class.Page):
     def __init__(self, state):
-        page_class.Page.__init__(self, state, 300, 700)
+        page_class.Page.__init__(self, state, 270, 350)
         self.man_var = StringVar(value=[])
-        self.manifest_info()
+        self.man_date_ent = None
+        self.man_num_ent = None
+        self.load_info = []
+        self.manifest_input()
+        self.store_input()
         self.added_manifests()
 
-    def manifest_info(self):
-        frame = Frame(self.root)
+    def manifest_input(self):
+        frame = Frame(self)
         frame.grid(row=0, column=0)
 
-        man_date_lbl = Label(master=frame, text="Manifest Date")
-        man_no_lbl = Label(master=frame, text="Manifest Number")
-
+        man_date_lbl = Label(master=frame, text='Manifest Date')
         man_date_lbl.grid(row=0, column=0)
-        man_no_lbl.grid(row=0, column=1)
 
-        man_date_ent = Entry(master=frame)
-        man_no_ent = Entry(master=frame)
-        man_date_ent.grid(row=1, column=0)
-        man_no_ent.grid(row=1, column=1)
+        man_num_lbl = Label(master=frame, text='Manifest Number')
+        man_num_lbl.grid(row=0, column=1)
 
-        str_no_lbl = Label(master=frame, text="Store Number")
-        str_no_lbl.grid(row=2, column=0)
+        self.man_date_ent = Entry(master=frame)
+        self.man_date_ent.grid(row=1, column=0, padx=5)
 
-        load_info = []
-        for i in range(4, 7):
-            str_no_ent = Entry(master=frame)
+        self.man_num_ent = Entry(master=frame)
+        self.man_num_ent.grid(row=1, column=1, padx=5)
+
+    def store_input(self):
+        frame = Frame(self)
+        frame.grid(row=1, column=0)
+
+        str_num_lbl = Label(frame, text="Store Number")
+        str_num_lbl.grid(row=0, column=0)
+
+        for i in range(1, 4):
+            str_no_ent = Entry(frame)
             str_no_ent.grid(row=i, column=0)
-            load_info.append(str_no_ent)
+            self.load_info.append(str_no_ent)
+        
+        def buttons():
+            add_ent_btn = Button(frame, text="Add Manifest", command=lambda : self.update_added_manifests(self.man_date_ent.get(), self.man_num_ent.get()))
+            add_ent_btn.grid(row=1, column=1, padx=20, sticky=(E, W))
 
-        add_ent_btn = Button(master=frame, text="Add Manifest", command=lambda : self.update_added_manifests(load_info, man_date_ent.get(), man_no_ent.get()))
-        add_ent_btn.grid(row=7, column=0)
+            del_ent_btn = Button(frame, text="Delete Last Entry", command=lambda : print("hi"))
+            del_ent_btn.grid(row=2, column=1, padx=20, sticky=(E, W))
 
-        del_ent_btn = Button(master=frame, text="Delete Last Entry", command=self.delete_manifest)
-        del_ent_btn.grid(row=7, column=1)
+            save_csv_btn = Button(frame, text='Save CSV', command=self.model.save_csv)
+            save_csv_btn.grid(row=3, column=1, padx=20, sticky=(E, W))
+        buttons()   
 
-        save_csv_btn = Button(master=frame, text='Save CSV', command=self.model.save_csv)
-        save_csv_btn.grid(row=7, column=2)
-
-        man_date_ent.focus()
-    
     def added_manifests(self):
-        frame = Frame(self.root)
-        frame.grid(row=8, column=0)
+        frame = Frame(self)
+        frame.grid(row=2, column=0, pady=10)
 
-        title_lbl = Label(master=frame, text='Added Manifests')
+        title_lbl = Label(frame, text='Added Manifests')
         title_lbl.grid(row=0, column=0)
 
-        listbox = Listbox(frame, height=10, width=50, listvariable=self.man_var)
+        listbox = Listbox(frame, width=50, listvariable=self.man_var)
 
-        scroll = Scrollbar(master=frame, orient=VERTICAL, command=listbox.yview)
+        scroll = Scrollbar(frame, orient=VERTICAL, command=listbox.yview)
         listbox.configure(yscrollcommand=scroll.set)
 
-        listbox.grid(row=1, column=0, columnspan=3)
-        scroll.grid(row=1, column=2)
-    
-    def update_added_manifests(self, load_info, date, num):
-        self.model.add_manifest(load_info, date, num)
+        listbox.grid(row=1, column=0, sticky=E)
+        scroll.grid(row=1, column=1, sticky=(N,S))
+
+    def update_added_manifests(self, date, num):
+        self.model.add_manifest(self.load_info, date, num)
         manifests = self.model.get_added_manifests()
-        show_manifests = [f'{manifests[i][6]} - ${manifests[i][8]}' for i in range(1, len(manifests))]
+        show_manifests = [f'{manifests[i][5]} - ${manifests[i][7]}' for i in range(1, len(manifests))]
         self.man_var.set(show_manifests)
 
     def delete_manifest(self):
         manifests = self.model.get_added_manifests()
         if len(manifests) > 1:
             manifests.pop()
-            show_manifests = [f'{manifests[i][6]} - ${manifests[i][8]}' for i in range(1, len(manifests))]
+            show_manifests = [f'{manifests[i][5]} - ${manifests[i][7]}' for i in range(1, len(manifests))]
             self.man_var.set(show_manifests)
 
