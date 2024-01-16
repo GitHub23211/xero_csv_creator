13bdf1d5c5764a882e5c62ecbb897b953d8f475a
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-01-16 16:00:45 +1100

    removed getters

--- pages/page_class.py
+++ pages/page_class.py
@@ -4,8 +4,8 @@ class Page(Frame):
     def __init__(self, state, width, height):
         Frame.__init__(self)
         self.state = state
-        self.model = state.get_model()
-        self.root = state.get_root()
+        self.root = state.root
+        self.model = state.model
         self.width = width
         self.height = height
         self.root.minsize(self.width, self.height)
