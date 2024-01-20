077d75eb1084bcdfe932f3533aab726b20870347
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-01-21 00:22:32 +1100

    removed router, state, implemented proper App class

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from atexit import register
 
 from pages import home
 import model
-import state
+
+class App(tk.Tk):
+    def __init__(self, model, page):
+        tk.Tk.__init__(self)
+        self.curr_frame = None
+        self.model = model
+        self.switch_frames(page)
+    
+    def switch_frames(self, new):
+        new_frame = new(self, self.model)
+        if self.curr_frame is not None:
+            self.curr_frame.destroy()
+        self.curr_frame = new_frame
+        self.curr_frame.grid()
 
 if __name__ == '__main__':
-    data = model.Model()
-    root = tk.Tk()
-    app_state = state.State(root, data)
-    app = home.Home(app_state)
-    register(data.cleanup)
-    root.mainloop()
+    model = model.Model()
+    app = App(model, home.Home)
+    register(model.cleanup)
+    app.mainloop()
--- router.py
+++
@@ -1,3 +0,0 @@
-def change_view(old, new, root):
-    old.destroy()
-    new(root)
