8186b73cda3f424c74c69e7f5d2b9d65ca2c69c2
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-03-19 01:46:58 +1100

    improved billing ui

--- pages/billing/b_ui.py
+++ pages/billing/b_ui.py
@@ -5,7 +5,6 @@ class billingUI(Frame):
         Frame.__init__(self, root)
 
     def build(self):
-        self.grid(row=0, column=0, sticky=('n', 's', 'e', 'w'))
         self.master.columnconfigure(0, weight=1)
         self.columnconfigure([i for i in range(0, 6)], weight=1)
         self.grid()
@@ -14,14 +13,15 @@ class billingUI(Frame):
         self.progress_bar()
     
     def date(self):
-        f = Frame(self)
-        f.grid(row=0, column=0, padx=10, pady=10)
-        Label(f, text='Start Work Date (dd/mm/yy)').grid(row=0, column=0)
-        Entry(f, textvariable=self.master.date_var).grid(row=0, column=1)
+        Label(self, text='Start Work Date (dd/mm/yy)').grid(row=0, column=0, padx=5, pady=5)
+        Entry(self, textvariable=self.master.date_var).grid(row=0, column=1, pady=5)
 
     def save_button(self):
-        Button(self, text='Create CSV', command=self.master.submit).grid(row=1, column=0, pady=10)
+        Button(self, text='Create CSV', command=self.master.submit).grid(row=3, column=0, columnspan=2, pady=8)
 
     def progress_bar(self):
-        Label(self, textvariable=self.master.prog_lbl_var).grid(row=2, column=0, pady=5)
-        ttk.Progressbar(self, orient='horizontal', length=200, mode='determinate', variable=self.master.prog_var, value=self.master.prog_var.get()).grid(row=3, column=0)
+        l = Label(self, textvariable=self.master.prog_lbl_var)
+        p = ttk.Progressbar(self, orient='horizontal', length=200, mode='determinate', variable=self.master.prog_var, value=self.master.prog_var.get())
+        
+        l.grid(row=1, column=0, columnspan=2, sticky=('w', 'e'), pady=2.5)
+        p.grid(row=2, column=0, columnspan=2, sticky=('w', 'e'), pady=2.5)
