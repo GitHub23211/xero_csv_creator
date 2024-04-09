0398b5f175667170d89a645c0a9247623320f002
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-04-09 23:18:34 +1000

    Change button variable names and add new button
    Make variable names more appropriate and add new button for a new window

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from tkinter import Tk, Button
 
-from pages.billing import billing
-from pages.local import local
+from pages.billing import Billing
+from pages.local import Local
+from pages.stores import Stores
 import model
 
 class App(Tk):
@@ -14,11 +15,12 @@ class App(Tk):
         self.create_menu()
 
     def create_menu(self):
-        l_tab = Button(self, text='Local Invoicing', command=lambda : self.open_window(local.Local, l_tab, 270, 350))
-        b_tab = Button(self, text='Billing', command=lambda: self.open_window(billing.Billing, b_tab, 320, 190))
+        local_btn = Button(self, text='Local Invoicing', command=lambda : self.open_window(Local, local_btn, 270, 350))
+        billing_btn = Button(self, text='Billing', command=lambda: self.open_window(Billing, billing_btn, 320, 190))
+        stores_btn = Button(self, text='Store Pricing', command=lambda: self.open_window(Stores, stores_btn, 320, 190))
 
-        l_tab.place(relx=0.5, rely=0.3, anchor='center', relwidth=0.5)
-        b_tab.place(relx=0.5, rely=0.6, anchor='center', relwidth=0.5)
+        local_btn.place(relx=0.5, rely=0.3, anchor='center', relwidth=0.5)
+        billing_btn.place(relx=0.5, rely=0.6, anchor='center', relwidth=0.5)
 
     def open_window(self, window, btn, width, height):            
         btn.config(state='disabled')
