41b6c6c3b15cd740ed5be2d7927a4d1511272783
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-04-09 12:11:36 +1000

    changed to more appropriate parameter name

--- src/components/top.py
+++ src/components/top.py
@@ -1,12 +1,12 @@
 from tkinter import Toplevel
 
 class Top(Toplevel):
-    def __init__(self, root, model, frame, width, height, close_func):
+    def __init__(self, root, model, frame, width, height, close_win_handler):
         Toplevel.__init__(self, root)
         self.curr_view = None
         self.model = model
         self.minsize(width, height)
-        self.protocol("WM_DELETE_WINDOW", close_func)
+        self.protocol("WM_DELETE_WINDOW", close_win_handler)
         self.switch_view(frame)
     
     def switch_view(self, new_frame):
