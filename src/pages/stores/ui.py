84c7159a9dbd2af060fa0949a128e21aab9bf6c4
Joshua Santos <joshua.santos1@students.mq.edu.au>
2024-04-16 00:49:19 +1000

    Show store pricings as a Treeview
    Remove list_item component in favour of a Treeview widget. Overall functionality is still a WIP.

--- src/pages/stores/ui.py
+++ src/pages/stores/ui.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label
+from tkinter import Frame, Label, Scrollbar, ttk
 from .components.list_item import ListItem
 
 class UI(Frame):
@@ -11,18 +11,21 @@ class UI(Frame):
         self.store_list()
 
     def store_list(self):
-        f = Frame(self)
-        f.grid(sticky='EW', padx=10, pady=10)
+        tree = ttk.Treeview(self)
 
-        f.columnconfigure((0, 2, 3, 4), weight=1)
-        f.columnconfigure(1, weight=4)
+        tree['columns'] = ('id', 'name', 'price')
+        tree.column('#0', width=0)
+        tree.heading('id', text='Store No.', anchor='w')
+        tree.heading('name', text='Store Name', anchor='w')
+        tree.heading('price', text='Store Pricing', anchor='w')
 
-        Label(f, text='Store No.').grid(row=0, column=0)
-        Label(f, text='Store Name').grid(row=0, column=1)
-        Label(f, text='Store Price').grid(row=0, column=2)
-        Label(f, text='Options').grid(row=0, column=3, columnspan=2)
+        for values in self.master.prices.values():
+            tree.insert('', 'end', text='', values=(values[0], values[1], f'${values[2]}'))
+        
+        scroll = Scrollbar(self, orient='vertical', command=tree.yview)
+        tree.config(yscrollcommand=scroll.set)
+        tree.config(height=20)
 
-        for i, value in enumerate(self.master.prices.values()):
-            ListItem(f, value[0], value[1], value[2], i+1)
-
+        scroll.grid(row=0, column=1, sticky='NS')
+        tree.grid(row=0, column=0, sticky='NSEW')
 
